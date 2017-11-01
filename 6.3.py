b2dc088ca0fdda80b6a878192c78adfb92febbaf
sxzyl <33241764+sxzyl@users.noreply.github.com>
2017-11-01 09:40:13 +0800

    Add files via upload

---
+++ 6.3.py
@@ -0,0 +1,6 @@
+var = 1
+while var == 1:
+    num = raw_input("Enter a number  :")
+    print "You entered: ", num
+
+print "Good bye!"
---
+++ 6.9.py
@@ -0,0 +1,3 @@
+var = 100
+if (var = 100): print "变量var的值为100"
+print "Good bye!"
